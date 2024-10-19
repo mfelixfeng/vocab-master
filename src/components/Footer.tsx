2a88a351b2523f384615a2921b03811e11948dcf
mfelixfeng <mfelixfeng@gmail.com>
2024-10-19 17:59:03 +0800

    refactor: refactor footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import { Box, Text } from "@chakra-ui/react";
 const Footer = () => {
   return (
     <Box as="footer" bg="gray.800" color="white" py={8} textAlign="center">
-      <Text>&copy; 2024 词达人. 保留所有权利.</Text>
+      <Text>&copy; 2024 词达人. All rights reserved.</Text>
     </Box>
   );
 };
